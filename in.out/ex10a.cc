b3e74fbd06d8fad75e9fd3613ff1a17e99717e31
Amiel Cohen <amielc1@gmail.com>
2024-05-22 21:14:00 +0300

    I put the files into a STRUCT

--- in.out/ex10a.cc
+++ in.out/ex10a.cc
@@ -2,44 +2,57 @@
 #include <fstream>
 #include <cstdlib> // use whith exit();
 #include <string>
+
 using namespace std;
 
-void open_file(string file_name, ifstream& file)
+struct Files
 {
-    file_name = "key1.txt";
-    cout << "file_name = key1.txt \n";
-    file.open(file_name);
+    ifstream file1;
+    ifstream file2;
+    string f1_name = "key1.txt";
+    string f2_name = "inp1.txt";
+};
+
+void open_file(ifstream& file, const string& filename)
+{
+    file.open(filename);
     if (!file)
     {
-        cerr << "The file not opened. \n";
-        exit(0);
+        cerr << "The file " << filename << " not opened.\n";
+        exit(1);
     }
-    cout << "The file was opened successfully\n";
+    cout << "The file " << filename << " was opened successfully.\n";
 }
 
-void count_row_in_file(ifstream& file, int& counter, string line)
+void open_files(Files& file)
+{
+    open_file(file.file1, file.f1_name);
+    open_file(file.file2, file.f2_name);
+}
+
+void count_row_in_file(Files& file, int& counter, string line)
 {
     counter = 0;
 
-    while (getline(file, line))
+    while (getline(file.file1, line))
         counter++;
 
     cout << "The number of lines is: " << counter << endl;
 
-    file.clear();
-    file.seekg(0, ios::beg); // Move to the beginning of the file
+    file.file1.clear();
+    file.file1.seekg(0, ios::beg); // Move to the beginning of the file
 }
 
-void create_arr(ifstream& file, int& counter, string line, string* arrKey)
+void create_arr(Files& file, int& counter, string line, string* arrKey)
 {
     for (int i = 0; i < counter; i++)
     {
-        getline(file, line);
+        getline(file.file1, line);
         arrKey[i] = line;
     }
 }
 
-string* create_arr_key(ifstream& file, int& counter)
+string* create_arr_key(Files& file, int& counter)
 {
     string line;
     count_row_in_file(file, counter, line);
@@ -52,19 +65,18 @@ string* create_arr_key(ifstream& file, int& counter)
 
 int main()
 {
-    ifstream file;
-    string file_name;
+    Files file;
     string* arrKey;
     int counter;
 
 
-    open_file(file_name, file);
+    open_files(file);
     arrKey = create_arr_key(file, counter);
 
-    /* for (int i = 0; i < counter; i++)
-     {
-         cout << arrKey[i] << endl;
-     }*/
+    //    for (int i = 0; i < counter; i++)
+    //{
+    //    cout << arrKey[i] << endl;
+    //}
 
     return 0;
 }
