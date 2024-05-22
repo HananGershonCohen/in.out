6ce1914df68ca98a8854cc886325203203dc41a2
Amiel Cohen <amielc1@gmail.com>
2024-05-22 22:35:50 +0300

    exercise 1 .In SD is perfect. By entering the names of input and output files by the user.

--- in.out/ex10a.cc
+++ in.out/ex10a.cc
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <fstream>
-#include <cstdlib> // use whith exit();
+#include <cstdlib> // use with exit();
 #include <string>
 
 using namespace std;
 
 struct Files
 {
-    ifstream file1;
-    ifstream file2;
-    string f1_name = "key1.txt";
-    string f2_name = "inp1.txt";
+    ifstream key;
+    ifstream txt;
+    ofstream out;
+    string key_name/* = "key1.txt"*/;
+    string txt_name/* = "inp1.txt"*/;
+    string out_name/* = "oup1.txt"*/;
 };
+
+void enter_name_file(Files& file)
+{
+   // cout << "please enter 3 name of files: \n";
+    cin >> file.key_name;
+    cin >> file.txt_name;
+    cin >> file.out_name;
+}
 
 void open_file(ifstream& file, const string& filename)
 {
@@ -21,33 +31,52 @@ void open_file(ifstream& file, const string& filename)
         cerr << "The file " << filename << " not opened.\n";
         exit(1);
     }
-    cout << "The file " << filename << " was opened successfully.\n";
+   // cout << "The file " << filename << " was opened successfully.\n";
 }
 
 void open_files(Files& file)
 {
-    open_file(file.file1, file.f1_name);
-    open_file(file.file2, file.f2_name);
+    open_file(file.key, file.key_name); // Changed file1 to key, and f1_name to key_name
+    open_file(file.txt, file.txt_name); // Changed file2 to txt, and f2_name to txt_name
+    file.out.open(file.out_name);
+    if (!file.out)
+    {
+        cerr << "The file " << file.out_name << " not opened.\n";
+        exit(1);
+    }
+   // cout << "The file " << file.out_name << " was opened successfully.\n";
+}
+
+void close_file(ifstream& file)
+{
+    file.close();
+}
+
+void close_files(Files& file)
+{
+    close_file(file.key);
+    close_file(file.txt);
+    file.out.close();
 }
 
 void count_row_in_file(Files& file, int& counter, string line)
 {
     counter = 0;
 
-    while (getline(file.file1, line))
+    while (getline(file.key, line)) // Changed file1 to key
         counter++;
 
-    cout << "The number of lines is: " << counter << endl;
+   // cout << "The number of lines is: " << counter << endl;
 
-    file.file1.clear();
-    file.file1.seekg(0, ios::beg); // Move to the beginning of the file
+    file.key.clear(); // Changed file1 to key
+    file.key.seekg(0, ios::beg); // Changed file1 to key
 }
 
 void create_arr(Files& file, int& counter, string line, string* arrKey)
 {
     for (int i = 0; i < counter; i++)
     {
-        getline(file.file1, line);
+        getline(file.key, line); // Changed file1 to key
         arrKey[i] = line;
     }
 }
@@ -61,7 +90,30 @@ string* create_arr_key(Files& file, int& counter)
     return arrKey;
 }
 
+void  swap(Files& file, int counter, char ch, string* arrKey)
+{
+    for (int i = 0; i < counter; i++)
+    {
+        if (ch == arrKey[i][0])
+        {
+            file.out << arrKey[i][1];
+            cout << arrKey[i][1];
+            return;
+        }
+    }
 
+    file.out << ch;
+    cout << ch;
+}
+
+void swap_file_text(Files& file, string* arrKey, int counter)
+{
+    char ch;
+    while (file.txt.get(ch))
+    {
+        swap(file, counter, ch, arrKey);
+    }
+}
 
 int main()
 {
@@ -69,14 +121,11 @@ int main()
     string* arrKey;
     int counter;
 
-
+    enter_name_file(file);
     open_files(file);
     arrKey = create_arr_key(file, counter);
-
-    //    for (int i = 0; i < counter; i++)
-    //{
-    //    cout << arrKey[i] << endl;
-    //}
+    swap_file_text(file, arrKey, counter);
+    close_files(file);
 
     return 0;
 }
