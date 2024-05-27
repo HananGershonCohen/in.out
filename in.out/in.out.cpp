accb475cc70f7d70c07fd98be6f14031f7ff7894
Amiel Cohen <amielc1@gmail.com>
2024-05-27 18:36:20 +0300

    An attempt failed to continue  'C; ex.

--- in.out/in.out.cpp
+++ in.out/in.out.cpp
@@ -11,9 +11,9 @@ struct Files
     ifstream txt1;
     ifstream txt2;
     ofstream out;
-    string name_txt1 = "ex10c_inp1.txt";
-    string name_txt2 = "ex10c_inp2.txt";
-    string name_out = "ex10_oup.txt";
+    string name_txt1 = /*"ex10c_inp1.txt";*/ "new1.txt";
+    string name_txt2 =/* "ex10c_inp2.txt";*/  "new2.txt";
+    string name_out = "ex10_oup.txt"; 
 };
 
 void open_file(ifstream& file, const string& filename)
@@ -51,44 +51,33 @@ void print_line(Files& file, bool exist,string &str1)
     {
         cout << str1;
         getline(file.txt1, str1);
-        cout << str1;
+        cout << str1 << endl;
     }
     else
         getline(file.txt1, str1);
 }
 
-
-
 void merger(Files& file, string str1, string str2)
 {
-    cout << str1; // dana
+    cout << str1;
     file.txt1 >> str1;
     file.txt2 >> str2;
-    while (file.txt1.peek() != '\n' && file.txt1.peek() != EOF && file.txt2.peek() != '\n' && file.txt2.peek() != EOF)
-    {
-        
 
+
+    while (file.txt1.peek() != '\n' /*&& file.txt1.peek() != EOF*/ && file.txt2.peek() != '\n' /*&& file.txt2.peek() != EOF*/)
+    {
         if (str1 > str2)
         {
-            cout << str1;
+            cout << ' ' << str1;
             file.txt1 >> str1;
         }
         else
         {
-            cout << str2;
+            cout << ' ' << str2;
             file.txt2 >> str2;
         }
     }
-    while (file.txt1.peek() != '\n' && file.txt1.peek() != EOF)
-    {
-        cout << str1;
-        file.txt1 >> str1;
-    }
-    while (file.txt2.peek() != '\n' && file.txt2.peek() != EOF)
-    {
-        cout << str2;
-        file.txt2 >> str2;
-    }
+    cout << endl;
 }
 
 void check_string(Files &file, string str1, string  str2, bool &exist)
@@ -98,27 +87,65 @@ void check_string(Files &file, string str1, string  str2, bool &exist)
         if (str1 == str2)
         {
             exist = true;
-            merger(file,str1, str2);
+            merger(file,str1,str2);
             return;
         }
     }
+    
 }
+
 
 void comparing_files(Files& file)
 {
     string str1, str2;
+    bool exist;
     while (file.txt1 >> str1)
     {
-        bool exist = false;
+        exist = false;
         check_string(file, str1, str2, exist);
-
         file.txt2.clear(); // Clear any potential EOF flags
         file.txt2.seekg(0, ios::beg);
 
         // move to the next line
         print_line(file, exist, str1);
-     
-      
+
+    }
+  
+}
+
+void check_different(Files& file, string str1, string str2)
+{
+    bool exist = false;
+    while (file.txt1 >>  str1)
+    {
+        if (str1 == str2)
+        {
+            exist = true;
+            getline(file.txt1, str1);
+        }
+         
+    }
+    if (!exist)
+    {
+        cout << str2 << ' ';
+        getline(file.txt2, str2);
+        cout << str2 << endl;
+    }
+   
+}
+
+void print_row_different(Files& file)
+{
+  
+    file.txt2.clear(); // Clear any potential EOF flags
+    file.txt2.seekg(0, ios::beg);
+    string str1, str2;
+
+    while (file.txt2 >> str2)
+    {
+        file.txt1.clear(); // Clear any potential EOF flags
+        file.txt1.seekg(0, ios::beg);
+        check_different(file, str1, str2);
     }
 }
 
@@ -128,6 +155,7 @@ int main()
     
     open_files(file);
     comparing_files(file);
+    print_row_different(file);
     close_files(file);
  
     return 0;
