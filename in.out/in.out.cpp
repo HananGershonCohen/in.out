c539c92567c75238a1f7b9abf62bc9d81d78a7d7
Amiel Cohen <amielc1@gmail.com>
2024-05-28 19:04:54 +0300

    EX C temp

--- in.out/in.out.cpp
+++ in.out/in.out.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <cstdlib> // use with exit();
 #include <string>
+#include <sstream> // for stringstream
+
 
 
 using namespace std;
@@ -11,9 +13,9 @@ struct Files
     ifstream txt1;
     ifstream txt2;
     ofstream out;
-    string name_txt1 = /*"ex10c_inp1.txt";*/ "new1.txt";
-    string name_txt2 =/* "ex10c_inp2.txt";*/  "new2.txt";
-    string name_out = "ex10_oup.txt"; 
+    string name_txt1 =/* "ex10c_inp1.txt";*/ "new1.txt";
+    string name_txt2 = /*"ex10c_inp2.txt"; */ "new2.txt";
+    string name_out = "ex10_oup.txt";
 };
 
 void open_file(ifstream& file, const string& filename)
@@ -24,7 +26,7 @@ void open_file(ifstream& file, const string& filename)
         cerr << "The file " << filename << " not opened.\n";
         exit(1);
     }
-     cout << "The file " << filename << " was opened successfully.\n";
+    cout << "The file " << filename << " was opened successfully.\n";
 }
 void open_files(Files& file)
 {
@@ -36,7 +38,7 @@ void open_files(Files& file)
         cerr << "The file " << file.name_out << " not opened.\n";
         exit(1);
     }
-     cout << "The file " << file.name_out << " was opened successfully.\n";
+    cout << "The file " << file.name_out << " was opened successfully.\n";
 }
 void close_files(Files& file)
 {
@@ -45,118 +47,77 @@ void close_files(Files& file)
     file.out.close();
 }
 
-void print_line(Files& file, bool exist,string &str1)
-{
-    if (!exist)
-    {
-        cout << str1;
-        getline(file.txt1, str1);
-        cout << str1 << endl;
-    }
-    else
-        getline(file.txt1, str1);
-}
-
-void merger(Files& file, string str1, string str2)
-{
-    cout << str1;
-    file.txt1 >> str1;
-    file.txt2 >> str2;
-
-
-    while (file.txt1.peek() != '\n' /*&& file.txt1.peek() != EOF*/ && file.txt2.peek() != '\n' /*&& file.txt2.peek() != EOF*/)
-    {
-        if (str1 > str2)
-        {
-            cout << ' ' << str1;
-            file.txt1 >> str1;
-        }
-        else
-        {
-            cout << ' ' << str2;
-            file.txt2 >> str2;
-        }
-    }
-    cout << endl;
-}
-
-void check_string(Files &file, string str1, string  str2, bool &exist)
-{
-    while (file.txt2 >> str2)
-    {
-        if (str1 == str2)
-        {
-            exist = true;
-            merger(file,str1,str2);
-            return;
-        }
-    }
-    
-}
-
 
 void comparing_files(Files& file)
 {
-    string str1, str2;
+    string line1, line2, str1, str2;
     bool exist;
-    while (file.txt1 >> str1)
+    while (getline(file.txt1, line1)) // Read the first line from the first file
     {
         exist = false;
-        check_string(file, str1, str2, exist);
+        // cout << line1 << endl;
+         // כעת אני רוצה לבדוק אם קיים בקובץ השני שם  כזה
+         // Use stringstream to extract the first word
+        stringstream ss1(line1);
+        ss1 >> str1;
+        while (getline(file.txt2, line2))
+        {
+            stringstream ss2(line2);
+            ss2 >> str2;
+
+            // אם נמצאה שורה שווה אז נדפיס את כל השורה שבקובץ הראשון ואת הציונים בלבד שבקובץ השני
+            if (str1 == str2)
+            {
+                exist = true;
+                cout << line1;
+                while (ss2 >> str2)
+                {
+                    cout << ' ' << str2;
+                }
+                cout << endl;
+            }
+
+        }
+        if (!exist)
+            cout << line1 << endl;
         file.txt2.clear(); // Clear any potential EOF flags
         file.txt2.seekg(0, ios::beg);
-
-        // move to the next line
-        print_line(file, exist, str1);
-
     }
-  
-}
 
-void check_different(Files& file, string str1, string str2)
-{
-    bool exist = false;
-    while (file.txt1 >>  str1)
+    // עכשיו החלק ההפוך של הפונקציה 
+    file.txt1.clear(); // Clear any potential EOF flags
+    file.txt1.seekg(0, ios::beg);
+
+    while (getline(file.txt2, line2))
     {
-        if (str1 == str2)
+        stringstream ss2(line2);
+        ss2 >> str2;
+        exist = false;
+        while (getline(file.txt1, line1))
         {
-            exist = true;
-            getline(file.txt1, str1);
+            stringstream ss1(line1);
+            ss1 >> str1;
+            if (str1 == str2)
+            {
+                exist = true;
+            }
         }
-         
-    }
-    if (!exist)
-    {
-        cout << str2 << ' ';
-        getline(file.txt2, str2);
-        cout << str2 << endl;
-    }
-   
-}
-
-void print_row_different(Files& file)
-{
-  
-    file.txt2.clear(); // Clear any potential EOF flags
-    file.txt2.seekg(0, ios::beg);
-    string str1, str2;
-
-    while (file.txt2 >> str2)
-    {
+        if (!exist)
+            cout << line2 << endl;
         file.txt1.clear(); // Clear any potential EOF flags
         file.txt1.seekg(0, ios::beg);
-        check_different(file, str1, str2);
     }
 }
 
 int main()
 {
     Files file;
-    
+
     open_files(file);
     comparing_files(file);
-    print_row_different(file);
+
     close_files(file);
- 
+
+
     return 0;
 }
